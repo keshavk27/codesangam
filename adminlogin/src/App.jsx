f8870bac3092ebb70ebfcb5f5185f9206455209b
keshavk27 <keshavk.2027@gmail.com>
2024-10-29 02:14:58 +0530

    adding adminlogin file

---
+++ adminlogin/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AdLogin from './adminlogin';
+
+function App() {
+  return (
+    <div className="App">
+      <AdLogin />
+    </div>
+  );
+}
+
+export default App;
